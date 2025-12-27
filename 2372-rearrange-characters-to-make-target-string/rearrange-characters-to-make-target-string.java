9400244feedc01bf4394bef70f5452871ca045f6
AAKASH THAKKAR <thakkaraakash101@gmail.com>
2025-12-28 02:56:58 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (48.51%) - LeetSync

---
+++ 2372-rearrange-characters-to-make-target-string/rearrange-characters-to-make-target-string.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int rearrangeCharacters(String s, String target) {
+        int[] freqS = new int[26];
+        int[] freqT = new int[26];
+
+        for(char ch : s.toCharArray()){
+            freqS[ch - 'a']++;
+        }
+        for(char ch : target.toCharArray()){
+            freqT[ch - 'a']++;
+        }
+
+        int result = Integer.MAX_VALUE;
+        for(int i = 0; i < 26; i++){
+            if(freqT[i] > 0){
+                result = Math.min(result, freqS[i] / freqT[i]);
+            }
+        }
+        return result;
+    }
+}
