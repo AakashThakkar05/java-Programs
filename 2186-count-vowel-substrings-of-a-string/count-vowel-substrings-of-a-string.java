1c9b35cf52caa7ea7910516fc7e388fc2665940a
AAKASH THAKKAR <thakkaraakash101@gmail.com>
2025-12-28 03:23:56 +0530

    Time: 11 ms (54.26%) | Memory: 44 MB (30.96%) - LeetSync

---
+++ 2186-count-vowel-substrings-of-a-string/count-vowel-substrings-of-a-string.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int countVowelSubstrings(String word) {
+        int count = 0;
+        int n = word.length();
+
+        for (int i = 0; i < n; i++) {
+
+            HashSet<Character> set = new HashSet<>();
+
+            for (int j = i; j < n; j++) {
+
+                char ch = word.charAt(j);
+                // consonant mila to break
+                if (!isVowel(ch)) {
+                    break;
+                }
+                // vowel ko set me daalo
+                set.add(ch);
+                // agar 5 vowels mil gaye
+                if (set.size() == 5) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    private boolean isVowel(char ch) {
+        return ch == 'a' || ch == 'e' || ch == 'i'
+            || ch == 'o' || ch == 'u';
+    }
+}
