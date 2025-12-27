04976e0bd7f4155e4b6182c9d9ee91b0fa3bb8bc
AAKASH THAKKAR <thakkaraakash101@gmail.com>
2025-12-28 02:23:00 +0530

    Time: 1 ms (99.04%) | Memory: 43.2 MB (7.68%) - LeetSync

---
+++ 2401-count-asterisks/count-asterisks.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countAsterisks(String s) {
+        int bar = 0;
+        int star = 0;
+
+        for(char ch : s.toCharArray()){
+            if(ch == '|'){
+                bar++;
+            }
+            if(ch == '*' && bar%2==0){
+                star++;
+            }
+        }
+        return star;
+    }
+}
